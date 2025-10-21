e28866e4da5019e4bb74ee61ebb20919f414afe4
Rohith Edwin <rohithedwin28193@gmail.com>
2025-10-21 11:05:21 +0530

    Changes

--- middleware.js
+++ middleware.js
@@ -8,7 +8,7 @@ module.exports.isLoggedIn = (req, res, next) => {
   if (req.isAuthenticated()) {
     return next();
   }
-  //req.flash("error", "You must be logged in first!");
+  req.flash("error", "You must be logged in first!");
   res.redirect("/login");
 };
 
